97e0c01f4610432e7c5252c06555015e22693d50
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-03-12 17:08:31 -0400

    first attempt at redoing to get rvalue references to work

--- tuple_call_unpack.h
+++ tuple_call_unpack.h
@@ -4,41 +4,50 @@
 #include<functional>
 #include<iostream>
 
-template<size_t ind, typename ret_t, typename tup_t, typename bound_t, typename...args_t>
-std::function<ret_t(args_t...)> bind(std::function<ret_t(bound_t,args_t...)> f, tup_t const& t)
+template<typename ret_t, typename tar_t, typename...args_t>
+std::function<ret_t(args_t...)> bind(std::function<ret_t(tar_t,args_t...)>&& f, tar_t&& t)
 {
     return
-        [=](args_t... args) -> ret_t 
+        [t{std::move(t)},f{std::move(f)}](args_t&&... args) -> ret_t 
         {
-            return f(std::get<ind>(t), args...);
+            return f(t, std::move(args)...);
         };
 }
 
-
-template<size_t ind, typename ret_t, typename tup_t, typename bound_t, typename...args_t>
-std::function<ret_t()> make_unpacked_call(std::function<ret_t(bound_t,args_t...)> f, tup_t const& t)
-{
-    return make_unpacked_call<ind+1,ret_t,tup_t,args_t...>(bind<ind,ret_t,tup_t,bound_t,args_t...>(f,t),t);
-}
-
-template<size_t ind, typename ret_t, typename tup_t, typename bound_t>
-std::function<ret_t()> make_unpacked_call(std::function<ret_t(bound_t)> f, tup_t const& t)
+template<size_t ind, typename ret_t, typename tup_t, typename arg_t>
+std::function<ret_t()> make_unpacked_call(std::function<ret_t(arg_t)>&& f, tup_t&& t)
 {
     return
-        [=]()-> ret_t
+        [t{std::move(t)},f{std::move(f)}]() -> ret_t
         {
-            return f(std::get<ind>(t));
+            return f(std::move(std::get<ind>(std::move(t))));
         };
 }
+
+template<size_t ind, typename ret_t, typename tup_t, typename arg_t, typename...args_t>
+std::function<ret_t()> make_unpacked_call(std::function<ret_t(arg_t,args_t...)>&& f, tup_t&& t)
+{
+    return make_unpacked_call<ind+1,ret_t,tup_t,args_t...>
+    (
+        bind<ret_t,typename std::tuple_element<ind,tup_t>::type,args_t...>
+        (std::move(f),std::move(std::get<ind>(t))),
+        std::move(t)
+    );
+}
+
+
+
+
+
 
 template<typename ret_t, typename...args_t>
-ret_t call(std::function<ret_t(args_t...)> const& f, std::tuple<args_t...> const& t)
+ret_t call(std::function<ret_t(args_t...)>&& f, std::tuple<args_t...>&& t)
 {
-    return make_unpacked_call<0, ret_t, std::tuple<args_t...>, args_t...>(f,t)();
+    return make_unpacked_call<0, ret_t, std::tuple<args_t...>, args_t...>(std::move(f),std::move(t))();
 }
 
 template<typename ret_t>
-ret_t call(std::function<ret_t()> const& f, std::tuple<> const& t)
+ret_t call(std::function<ret_t()>&& f, std::tuple<>&& t)
 {
     return f();
 }
