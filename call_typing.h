2ade041d1058000fcf738bf93bf1c04459f4f364
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-04-01 13:05:07 -0400

    function reference arguments now work (and neatened up call typing)

--- call_typing.h
+++ call_typing.h
@@ -47,16 +47,57 @@ constexpr auto as_storable()
     }
 }
 
+template<typename t>
+constexpr auto as_passable()
+{
+
+    if constexpr (std::template is_rvalue_reference<t>::value || std::template is_rvalue_reference<t>::value)
+    {
+        return type_wrap<t>();
+    }
+    else
+    {
+        return type_wrap<t const&>();
+    }
+}
+
+
+
+
+#ifdef _MSVC_VER
+
 //remove reference is not nessecary for actual logic, it is only needed for vc17
 template<typename t>
 using store_t = typename std::remove_reference_t<decltype(as_storable<t>())>::held;
 
+
+//remove reference is not nessecary for actual logic, it is only needed for vc17
 template<typename t>
-constexpr auto move_or_deref(store_t<t>& x)
+using pass_t = typename std::remove_reference_t<std::add_lvalue_reference<decltype(as_passable<t>())>>::held;
+
+#else
+
+template<typename t>
+using store_t = typename decltype(as_storable<t>())::held;
+
+template<typename t>
+using pass_t = typename decltype(as_passable<t>())::held;
+
+#endif
+
+
+
+
+template<typename t>
+constexpr pass_t<t> move_or_deref(store_t<t>& x)
 {
     if constexpr(std::is_rvalue_reference<t>::value && !std::is_const<typename std::remove_reference<t>::type>::value)
     {
         return std::move(x);
+    }
+    else if constexpr(!std::is_reference<t>::value)
+    {
+        return std::move(*x);
     }
     else
     {
